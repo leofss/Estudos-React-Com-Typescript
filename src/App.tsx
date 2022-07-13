475f97ab2497b559e6be4289ec3a200e282cf95b
leofss <leofernandess11@gmail.com>
2022-07-13 15:56:45 -0300

    Variaveis, funcoes, components, importacao e uso component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+//4 - importação de componentes
+import FirstComp from './Components/FirstComponent';
 
 function App() {
+  //1 variaveis
+  //{} = execução de JS 
+  //&& = concatenação com AND
+
+  const name : string = "Leonardo";
+  const age : number = 30;
+  const IsWorking : boolean = true;
+
+  //2 - funções
+
+  const UserGreeting = (name : string): string =>{
+    return `Olá ${name}`
+  }
+
   return (
     <div className="App">
-      <h1>TypeScript com Reactt</h1>
+      <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>idade: {age}</p>
+      {IsWorking && (
+        <p>Está trabalhando</p>
+      )}
+      <h3>{UserGreeting(name)}</h3>
+      <FirstComp />
     </div>
   );
 }
---
+++ src/Components/FirstComponent.tsx
@@ -0,0 +1,15 @@
+//3 - component 
+import React, { ReactElement } from "react";
+
+
+
+function FirstComp() : ReactElement {
+    return (
+        <div>
+            <h1> Meu Primeiro Componente</h1>
+        </div>
+    )
+}
+
+
+export default FirstComp
